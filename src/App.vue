58104d7bbb8fba16befcb7f2502dacd133c209f6
Adri Silva <adri.silva@csktech.com.br>
2018-12-18 17:22:11 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <ul class="left hide-on-med-and-down">
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Categorias</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Categoria</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Editar</th>
+          <th>Excluir</th>
+        </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="product of products" :key="product.id">
+            <td>{{ product.name }}</td>
+            <td>{{ product.category }}</td>
+            <td>R$ {{ product.price | moneyFormat}}</td>
+            <td>{{ product.quantity }}</td>
+            <td><a class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></a></td>
+            <td><a class="waves-effect btn-small red darken-1 red"><i class="material-icons left">delete_sweep</i></a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Products from './services/products';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+
+        mounted(){
+            Products.list().then(response => {
+                this.products = response.data.products;
+            });
+        },
+
+        data(){
+            return {
+                products: []
+            }
+        },
+
+        filters: {
+            moneyFormat: function(value){
+                let val = (value/1).toFixed(2).replace('.', ',')
+                return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+            }
+        }
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
